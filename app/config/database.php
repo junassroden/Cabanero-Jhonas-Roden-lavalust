91511de775f468ca24dce151b66ef3cba0022df7
Jhonas Cabanero <cabanerojhoas@gmail.com>
2026-09-03 18:01:18 +0800

    Updated codes

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv('DB_HOST') ?: 'localhost',
+    'hostname'	=> getenv('DB_HOST') ?: '',
     'port'		=> getenv('DB_PORT') ?: '3306',
-    'username'	=> getenv('DB_USERNAME') ?: 'root',
+    'username'	=> getenv('DB_USERNAME') ?: '',
     'password'	=> getenv('DB_PASSWORD') ?: '',
-    'database'	=> getenv('DB_NAME') ?: 'mydb',
+    'database'	=> getenv('DB_NAME') ?: 'utf8mb4',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
